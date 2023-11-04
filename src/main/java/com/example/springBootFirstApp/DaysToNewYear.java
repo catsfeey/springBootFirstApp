3372fbc9b2fdb581adbbdf2b537261532761dacf
catsfeey <catrionafeehily@outlook.com>
2023-11-04 11:11:31 +0800

    Initial commit

---
+++ src/main/java/com/example/springBootFirstApp/DaysToNewYear.java
@@ -0,0 +1,22 @@
+package com.example.springBootFirstApp;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@RestController
+
+public class DaysToNewYear {
+
+    @GetMapping("/")
+    public String index() {
+
+        //return "Greetings from Spring Boot!";
+        LocalDate fromDate = LocalDate.now();
+        LocalDate newYear = fromDate.plusYears(1).withDayOfYear(1);
+        return "There are " + Days.daysBetween(fromDate, newYear) + "days until the new year!";
+    }
+
+}
